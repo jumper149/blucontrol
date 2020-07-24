50ce205642a54fb6cff4dc220f1c3bcac140deaa
Felix Springer <felixspringer149@gmail.com>
2020-07-24 16:43:33 +0200

    remove unnecessary imports

--- src/Blumon/Control.hs
+++ src/Blumon/Control.hs
@@ -3,13 +3,12 @@ module Blumon.Control (
 ) where
 
 import Control.Concurrent
-import Control.Monad.Base
 import qualified Data.Text as T
 
 import Blumon.Config
 import Blumon.Monad.Recolor
 
-loopRecolor :: (MonadBase IO m, MonadRecolor m) => Config -> (forall a. m a -> IO (Either T.Text a)) -> IO ()
+loopRecolor :: MonadRecolor m => Config -> (forall a. m a -> IO (Either T.Text a)) -> IO ()
 loopRecolor conf run = do
   recolorSuccess <- run recolor
   case recolorSuccess of
