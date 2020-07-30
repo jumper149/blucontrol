07561083cb40ee1e3afd5b362139f06468e0bd3c
Felix Springer <felixspringer149@gmail.com>
2020-07-30 20:20:21 +0200

    modify default.nix, so now it uses the default ghc and also uses this ghc with the necessary modules to compile the use config

--- default.nix
+++ default.nix
@@ -1,2 +1,24 @@
-with import <nixpkgs> {};
-pkgs.haskell.packages.ghc883.callCabal2nix "blumon" ./. {}
+let
+  overlay = self: super: {
+    haskellPackages = super.haskellPackages.extend (
+      self: super: {
+        blumon = super.callCabal2nix "blumon" ./. {};
+      }
+    );
+  };
+in
+  with import <nixpkgs> { overlays = [ overlay ]; };
+  let
+    blumonEnv' = packages: pkgs.haskellPackages.ghcWithPackages (self: [ self.blumon ] ++ packages self);
+    blumonEnv = blumonEnv' (_: []);
+  in
+    stdenv.mkDerivation {
+      name = "blumon-with-packages-${pkgs.haskellPackages.blumon.version}-${blumonEnv.version}";
+      nativeBuildInputs = [ makeWrapper ];
+      buildCommand = ''
+        makeWrapper ${blumonEnv}/bin/blumon $out/bin/blumon \
+          --prefix PATH : ${lib.makeBinPath [ blumonEnv ]}
+      '';
+      preferLocalBuild = true;
+      allowSubstitues = false;
+    }
