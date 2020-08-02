8b876d728810fb1b95acd1e8e4bde49ef9b9908f
Felix Springer <felixspringer149@gmail.com>
2020-08-02 16:59:32 +0200

    fix haddock linking

--- src/Bludigon/Control.hs
+++ src/Bludigon/Control.hs
@@ -10,14 +10,14 @@ import Data.Kind
 class MonadBaseControl IO m => MonadControl m where
 
   {- | Give a constraint to allow 'doInbetween' to handle a polymorphic argument.
-    This is usfeul to allow arguments wrapped in monadic state 'StM' from running 'MonadGamma' and
-    'MonadRecolor'.
+    This is usfeul to allow arguments wrapped in monadic state 'StM' from running
+    'Bludigon.Gamma.MonadGamma' and 'Bludigon.Recolor.MonadRecolor'.
   -}
   type ControlConstraint m a :: Constraint
 
   -- | This function will be called after recoloring the screen.
   doInbetween :: ControlConstraint m a
-              => a -- ^ the returned value from the last call of 'recolor' including monadic state
+              => a -- ^ the returned value from the last call of 'Bludigon.Recolor.recolor' including monadic state
               -> m () -- ^ the side effect to be run inbetween recoloring
 
 instance MonadControl IO where
