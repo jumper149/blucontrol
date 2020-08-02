f49816e93ab54b71c2bdcdd3bc7b2b1ead90c7f6
Felix Springer <felixspringer149@gmail.com>
2020-08-02 18:55:24 +0200

    make the module Bludigon a sort of Prelude

--- src/Bludigon.hs
+++ src/Bludigon.hs
@@ -1,12 +1,46 @@
 module Bludigon (
-  module Bludigon.Main
-, module Bludigon.RGB
-, module Bludigon.RGB.Temperature
+
+-- * main
+{- | For most configurations 'bludigon' should be called directly from the @main@ function in
+   @$XDG_CONFIG_HOME\/bludigon\/bludigon.hs@
+-}
+  bludigon
+{- | 'ConfigControl' will set the monads in which recoloring and calculation of the gamma values
+  will take place.
+-}
+, ConfigControl (..)
+
+-- * RGB
+-- | RGB values are represented by 'Trichromaticity'.
+, Trichromaticity (..)
+
+{- | An alternative way to declaring 'Trichromaticity' directly is to use 'Temperature' and the
+  conversion function 'temperature'.
+-}
+, Temperature
+, temperature
+
+-- * Control
+-- | Modules with instances of 'MonadControl' can be found under @Bludigon.Control.*@.
+, MonadControl (..)
+
+-- * Gamma
+-- | Modules with instances of 'MonadGamma' can be found under @Bludigon.Gamma.*@.
+, MonadGamma (..)
+
+-- * Recolor
+-- | Modules with instances of 'MonadRecolor' can be found under @Bludigon.Recolor.*@.
+, MonadRecolor (..)
+
+-- * other
 , Default (..)
 ) where
 
 import Data.Default
 
+import Bludigon.Control
+import Bludigon.Gamma
 import Bludigon.Main
+import Bludigon.Recolor
 import Bludigon.RGB
 import Bludigon.RGB.Temperature
