c27c39eaee6c4a94615ad49915a66d316386878d
Felix Springer <felixspringer149@gmail.com>
2020-08-15 01:01:25 +0200

    improve setting environment variables

--- dev/shell.nix
+++ dev/shell.nix
@@ -15,16 +15,25 @@ in
     shellHook = ''
       EXPORTS="$(cat $(which ghc) | grep '^export' | sed "s/'//g")"
 
+      _getExportedVariable() {
+        echo "$EXPORTS" | grep "^export $1=" | sed "s/export $1=//g"
+      }
+
       echo "found exports:"
       echo "$EXPORTS"
 
-      _getExportedVariable() {
-        echo "$EXPORTS" | grep "^export $1" | sed "s/export $1=//g"
-      }
+      echo "applying exports:"
 
       export NIX_GHC="$(_getExportedVariable "NIX_GHC")"
+      echo "export NIX_GHC=$NIX_GHC"
+
       export NIX_GHCPKG="$(_getExportedVariable "NIX_GHCPKG")"
+      echo "export NIX_GHCPKG=$NIX_GHCPKG"
+
       export NIX_GHC_DOCDIR="$(_getExportedVariable "NIX_GHC_DOCDIR")"
+      echo "export NIX_GHC_DOCDIR=$NIX_GHC_DOCDIR"
+
       export NIX_GHC_LIBDIR="$(_getExportedVariable "NIX_GHC_LIBDIR")"
+      echo "export NIX_GHC_LIBDIR=$NIX_GHC_LIBDIR"
     '';
   }
