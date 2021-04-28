58cb33a72b92e968e94c726f30ba56d82ed1cf65
Felix Springer <felixspringer149@gmail.com>
2021-04-28 21:06:48 +0200

    remove exported variables from shell.nix, use haskellPackages.shellFor instead of mkShell, which also allows for hoogle in scope

--- dev/shell.nix
+++ dev/shell.nix
@@ -1,40 +1,17 @@
-with import <nixpkgs> {};
 let
-  packages = haskell.packages.ghc8104;
-  blucontrol = packages.callCabal2nix "blucontrol" ../. {};
+  nixpkgs = import <nixpkgs> {};
+  haskellPackages = nixpkgs.haskell.packages.ghc8104;
+  blucontrol = p: p.callCabal2nix "blucontrol" ../. {};
 in
-  pkgs.mkShell {
-    buildInputs = with packages; [
+  haskellPackages.shellFor {
+    buildInputs = with haskellPackages; [
       cabal-install
-      hlint
       haskell-language-server
+      hlint
       implicit-hie
     ];
-    inputsFrom = [
-      blucontrol.env
+    packages = p: [
+      (blucontrol p)
     ];
-    shellHook = ''
-      EXPORTS="$(cat $(which ghc) | grep '^export' | sed "s/'//g")"
-
-      _getExportedVariable() {
-        echo "$EXPORTS" | grep "^export $1=" | sed "s/export $1=//g"
-      }
-
-      echo "found exports:"
-      echo "$EXPORTS"
-
-      echo "applying exports:"
-
-      export NIX_GHC="$(_getExportedVariable "NIX_GHC")"
-      echo "export NIX_GHC=$NIX_GHC"
-
-      export NIX_GHCPKG="$(_getExportedVariable "NIX_GHCPKG")"
-      echo "export NIX_GHCPKG=$NIX_GHCPKG"
-
-      export NIX_GHC_DOCDIR="$(_getExportedVariable "NIX_GHC_DOCDIR")"
-      echo "export NIX_GHC_DOCDIR=$NIX_GHC_DOCDIR"
-
-      export NIX_GHC_LIBDIR="$(_getExportedVariable "NIX_GHC_LIBDIR")"
-      echo "export NIX_GHC_LIBDIR=$NIX_GHC_LIBDIR"
-    '';
+    withHoogle = true;
   }
