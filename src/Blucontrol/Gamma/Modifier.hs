a2e32a21002385649a4679b3ded86fce2597b512
Felix Springer <felixspringer149@gmail.com>
2021-04-30 12:25:12 +0200

    reorder instances in Blucontrol.Gamma.Modifier

--- src/Blucontrol/Gamma/Modifier.hs
+++ src/Blucontrol/Gamma/Modifier.hs
@@ -15,15 +15,15 @@ import Blucontrol.Gamma
 newtype GammaModifierT c m a = GammaModifierT { unGammaModifierT :: ReaderT (c -> IO c) m a }
   deriving (Applicative, Functor, Monad, MonadBase b, MonadBaseControl b, MonadTrans, MonadTransControl)
 
-instance (MonadBase IO m, MonadGamma c m) => MonadGamma c (GammaModifierT c m) where
-  gamma = do oldGamma <- lift gamma
-             modifyGamma <- GammaModifierT ask
-             liftBase $ modifyGamma oldGamma
-
 instance MonadReader r m => MonadReader r (GammaModifierT c m) where
   ask = lift ask
   local f tma = liftWith $ \ run ->
     local f $ run tma
+
+instance (MonadBase IO m, MonadGamma c m) => MonadGamma c (GammaModifierT c m) where
+  gamma = do oldGamma <- lift gamma
+             modifyGamma <- GammaModifierT ask
+             liftBase $ modifyGamma oldGamma
 
 runGammaModifierT :: (c -> IO c) -> GammaModifierT c m a -> m a
 runGammaModifierT modify tma = runReaderT (unGammaModifierT tma) modify
