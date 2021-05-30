a327eed419ec7466e55243c3c98470a731a385e2
Felix Springer <felixspringer149@gmail.com>
2021-05-30 21:40:13 +0200

    split up loopRecolor into multiple functions

--- src/Blucontrol/Main/Control.hs
+++ src/Blucontrol/Main/Control.hs
@@ -13,6 +13,8 @@ import Blucontrol.Gamma
 import Blucontrol.Recolor
 import Blucontrol.RGB
 
+-- | Run the loop, using `gamma`, `recolor` and `doInbetween`.
+-- The arguments are the actual monad runners.
 loopRecolor :: (ControlConstraint m (StM g (StM r ())), MonadBaseControl IO g, MonadBaseControl IO r, MonadControl m, MonadGamma c g, MonadRecolor r)
             => (forall a. g a -> IO (StM g a))
             -> (forall a. r a -> IO (StM r a))
@@ -21,19 +23,32 @@ loopRecolor runG runR = void $
   liftBaseWith $ \ runCIO ->
     runR $ liftBaseWith $ \ runRIO ->
       runG $ liftBaseWith $ \ runGIO -> do
-        let doRecolorGamma = do
-              runGIO $ do
-                rgb <- toRGB <$> gamma
-                liftBase $ runRIO $ recolor rgb
-            doLoopRecolor = do
-              lastResult <- get
-              _ <- liftBase $ runCIO $ doInbetween lastResult
-              nextResult <- liftBase doRecolorGamma
-              put nextResult
-              doLoopRecolor
-        firstResult <- doRecolorGamma
-        evalStateT doLoopRecolor firstResult
+        firstResult <- doRecolorGamma runGIO runRIO
+        evalStateT (doLoopRecolor runCIO runGIO runRIO) firstResult
 
+-- | Use `gamma` and give the result to `recolor`.
+-- The arguments are runners from `liftBaseWith`.
+doRecolorGamma :: (MonadBaseControl IO g, MonadBaseControl IO r, MonadGamma c g, MonadRecolor r)
+               => (forall a. g a -> IO (StM g a))
+               -> (forall a. r a -> IO (StM r a))
+               -> IO (StM g (StM r ()))
+doRecolorGamma runGIO runRIO = runGIO $ do
+  rgb <- toRGB <$> gamma
+  liftBase $ runRIO $ recolor rgb
+
+-- | A single iteration of `loopRecolor`.
+-- The arguments are runners from `liftBaseWith`.
+doLoopRecolor :: (ControlConstraint m (StM g (StM r ())), MonadBaseControl IO g, MonadBaseControl IO r, MonadControl m, MonadGamma c g, MonadRecolor r)
+              => (forall a. m a -> IO (StM m a))
+              -> (forall a. g a -> IO (StM g a))
+              -> (forall a. r a -> IO (StM r a))
+              -> StateT (StM g (StM r ())) IO ()
+doLoopRecolor runCIO runGIO runRIO = do
+  lastResult <- get
+  void $ liftBase $ runCIO $ doInbetween lastResult
+  nextResult <- liftBase $ doRecolorGamma runGIO runRIO
+  put nextResult
+  doLoopRecolor runCIO runGIO runRIO
 
 data ConfigControl m g r = ConfigControl { runControl :: forall a. m a -> IO a
                                          , runGamma   :: forall a. g a -> IO (StM g a)
