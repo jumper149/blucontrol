9f9bdd339df339c4b303f2ef9428bc73cf785bed
Felix Springer <felixspringer149@gmail.com>
2021-06-23 22:25:11 +0200

    rename runCIO to runC and the same for runGIO and runRIO

--- src/Blucontrol/Main/Control.hs
+++ src/Blucontrol/Main/Control.hs
@@ -18,28 +18,28 @@ loopRecolor :: (MonadBaseControl IO m, MonadBaseControl IO g, MonadBaseControl I
             -> RunInBase r IO
             -> (GammaValue g -> RecolorValue r)
             -> IO ()
-loopRecolor runCIO runGIO runRIO coerceValue = do
+loopRecolor runC runG runR coerceValue = do
 
       -- Use `gamma` and give the result to `recolor`.
       -- Then use the result of `recolor` and give it to `doInbetween` including the monadic state.
       -- The argument is an initial monadic state.
   let doRecolorGamma x =
-        runCIO $ do
+        runC $ do
           x1 <- restoreM x
           x4 <- liftBase $ do
-            runGIO $ do
+            runG $ do
               x2 <- restoreM x1
               value <- coerceValue <$> gamma
-              liftBase $ runRIO $ do
+              liftBase $ runR $ do
                 x3 <- restoreM x2
                 recolor value
                 pure x3
           let currentRecolorValue =
-                runGIO $ do
+                runG $ do
                     -- TODO: `unsafeCoerce` is necessary because of GHC limitations with type families.
                     -- `unsafeCoerce` will act like `id`.
                     x5 <- restoreM $ unsafeCoerce x4
-                    liftBase $ runRIO $ void $ restoreM x5
+                    liftBase $ runR $ void $ restoreM x5
           currentRecolorValue' <- liftBase currentRecolorValue
           doInbetween currentRecolorValue'
           pure x4
@@ -48,7 +48,7 @@ loopRecolor runCIO runGIO runRIO coerceValue = do
       doLoopRecolor x = doLoopRecolor =<< liftBase (doRecolorGamma x)
 
   -- Initialize the monadic state.
-  initStM <- runCIO $ liftBase $ runGIO $ liftBase $ runRIO $ pure undefined
+  initStM <- runC $ liftBase $ runG $ liftBase $ runR $ pure undefined
 
   -- Start an infinite loop.
   -- TODO: `unsafeCoerce` is necessary because of GHC limitations with type families.
