4217a0294d5b8bd90423ffc8aeccdb476ebc0154
Felix Springer <felixspringer149@gmail.com>
2021-07-18 14:24:42 +0200

    Update nixpkgs.

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,9 +1,9 @@
 let
-  commit = "0b8b127125e5271f5c8636680b6fe274844aaa9d";
+  commit = "2489eb5e4516aab575ab114b7e0a3e1b5c5daca7";
 
   # !!! Requires change, when the commit is changed !!!
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  hash = "1rjb1q28ivaf20aqj3v60kzjyi5lqb3krag0k8wwjqch45ik2f86";
+  hash = "0snsicv0i50d0cp2lhjgjqi8qkhv37lx9dy3sg6jqnr5mllwbgad";
 
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz";
