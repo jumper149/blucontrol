3c0b21a2bdf0f917a7881f86e1380fc3463b9037
Felix Springer <felixspringer149@gmail.com>
2021-08-15 22:30:16 +0200

    Add nix flake.

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Configurable blue light filter";
+
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixpkgs-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    checks.x86_64-linux.blucontrol = self.packages.x86_64-linux.blucontrol;
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.blucontrol;
+
+    packages.x86_64-linux.blucontrol =
+      with import nixpkgs { system = "x86_64-linux"; };
+      (import ./nix/build.nix).blucontrolWrapped { inherit stdenv lib makeWrapper nix-gitignore haskellPackages; };
+
+    devShell.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      (import ./nix/build.nix).blucontrolShell { inherit rnix-lsp haskellPackages nix-gitignore; };
+
+  };
+}
